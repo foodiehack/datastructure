5360497026da9deb6ce7a87b6e610f3c999d7440
foodiehack <tanmayag78@gmail.com>
2019-06-14 04:24:27 +0530

    Update selectionsort.c

--- sorting/selectionsort.c
+++ sorting/selectionsort.c
@@ -29,17 +29,18 @@ int main()
 	int i,n; 
 	printf("How many number do you want to enter:");
 	scanf("%d",&n);
-	int a[n];
+	//int a[n];  This is a wrong way as memory is given to array at compile time.
+	int *a = (int*)malloc(n*sizeof(int)); //dynamic memory allocation
 	printf("\nEnter the numbers \n");
  	for(i = 0; i < n; i++)
-    {
+    	{
         printf("%d :",i+1);
         scanf("%d",&a[i]);
-    }
-    selectionsort(a,n);
-    printf("Your sorted numbers: ");
-    for(i = 0; i < n; i++)
-        printf("%d ",a[i]);
-    printf("\n");
-    return 0;
+   	 }
+   	selectionsort(a,n);
+   	printf("Your sorted numbers: ");
+    	for(i = 0; i < n; i++)
+        	printf("%d ",a[i]);
+    	printf("\n");
+    	return 0;
 }
